17c7ad6179a02bd41499a2ad363056aae7b53e6c
smhrd <smhrd@220.71.97.201>
2023-07-18 11:35:23 +0900

    영준 dto 수정

--- PokerGame/src/Model/PokerDTO.java
+++ PokerGame/src/Model/PokerDTO.java
@@ -1,5 +1,24 @@
 package Model;
 
 public class PokerDTO {
-
+	// pattern이라는 카드의 모양
+	// number이라는 카드의 숫자
+	private String pattern;
+	private int number;
+	
+	// 생성자 메소드
+	public PokerDTO(String pattern, int number) {
+		super();
+		this.pattern = pattern;
+		this.number = number;
+	}
+	
+	// getter메소드
+	public String getPattern() {
+		return pattern;
+	}
+	public int getNumber() {
+		return number;
+	}
+	
 }
