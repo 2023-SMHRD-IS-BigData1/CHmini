03c0f08db2fa106e86c241809dd8ca5c9837dc36
smhrd <smhrd@218.157.38.54>
2023-07-18 11:51:58 +0900

    commit

--- PokerGame/src/Model/Card.java
+++ PokerGame/src/Model/Card.java
@@ -1,13 +1,13 @@
 package Model;
 
-public class PokerDTO {
+public class Card {
 	// pattern이라는 카드의 모양
 	// number이라는 카드의 숫자
 	private String pattern;
 	private int number;
 	
 	// 생성자 메소드
-	public PokerDTO(String pattern, int number) {
+	public Card(String pattern, int number) {
 		super();
 		this.pattern = pattern;
 		this.number = number;
